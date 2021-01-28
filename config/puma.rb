8368d448c9eef092cc106dfc4f926c61568f4daa
Dmytro Ababii <ababiids@gmail.com>
2021-01-28 16:11:54 +0200

    Fix puma bug

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,4 @@
+bind "unix:///var/run/puma/my_app.sock" unless Rails.env.development?
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
