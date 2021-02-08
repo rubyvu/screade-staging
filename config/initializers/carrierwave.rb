96b95388b003ed351551c4cb0db952e2e136cd9f
Dmytro Ababii <ababiids@gmail.com>
2021-02-08 17:42:30 +0200

    Update S3 bucket configure

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,14 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:               'AWS',
-    aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
+    aws_access_key_id:      ENV['AWS_S3_ACCESS_KEY_ID'],
+    aws_secret_access_key:  ENV['AWS_S3_SECRET_ACCESS_KEY'],
     region:                 ENV['AWS_REGION']
   }
-  if Rails.env.development?
-    config.fog_directory = ENV['AWS_S3_PUBLIC_BUCKET']
-    config.fog_public    = true
-  else
-    config.fog_directory  = ENV['AWS_S3_PRIVATE_BUCKET']
-    config.fog_public     = false
-    # Set URL expiration to 24 hours(in seconds)
-    config.fog_authenticated_url_expiration = 86400
-  end
+
+  config.fog_directory  = ENV['AWS_S3_PRIVATE_BUCKET']
+  config.fog_public     = false
+  # Set URL expiration to 24 hours(in seconds)
+  config.fog_authenticated_url_expiration = 86400
   config.cache_dir      = Rails.root.join('tmp', 'uploads')
 end
