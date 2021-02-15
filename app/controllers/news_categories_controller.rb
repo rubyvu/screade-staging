a4b37cfad981b3ac29095f04f24d474a8be2f4a9
Dmytro Ababii <ababiids@gmail.com>
2021-02-15 20:50:18 +0200

    Add styles for news categories bar

--- app/controllers/news_categories_controller.rb
+++ app/controllers/news_categories_controller.rb
@@ -2,5 +2,6 @@ class NewsCategoriesController < ApplicationController
   # GET /news_categories/:id
   def show
     @news_category = NewsCategory.find(params[:id])
+    @news_categories = NewsCategory.all
   end
 end
