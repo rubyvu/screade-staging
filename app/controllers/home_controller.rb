b82a7cb5cb14afeb6f376aa0c74744cbd9480c4d
Dmytro Ababii <ababiids@gmail.com>
2021-02-18 15:42:28 +0200

    Update Trends controller for Web

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,17 @@ class HomeController < ApplicationController
   # GET /home
   def index
     @home = {}
+    
+    # Breaking News
     @home[:breaking_news_title] = BreakingNews.find_by(is_active: true)&.title
+    
+    # Trends
+    @home[:trends] = []
+    NewsArticle.left_joins(:lits).group(:id).order('COUNT(lits.id) DESC').limit(6).each do |trend|
+      @home[:trends] << { url: trend.url, title: trend.title, img_url: trend.img_url }
+    end
+    
+    # News
     @home[:news_categories] = NewsCategory.all
   end
 end
