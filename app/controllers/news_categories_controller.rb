74ba136b18f401bcbf7a2a498f51d25c85b92317
Dmytro Ababii <ababiids@gmail.com>
2021-02-24 17:53:36 +0200

    Render NewsArticles by categories

--- app/controllers/news_categories_controller.rb
+++ app/controllers/news_categories_controller.rb
@@ -1,7 +1,19 @@
 class NewsCategoriesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show]
+  
   # GET /news_categories/:id
   def show
-    @news_category = NewsCategory.find(params[:id])
-    @news_categories = NewsCategory.all
+    # News Categories
+    news_category = NewsCategory.find(params[:id])
+    @category = {}
+    @category[:news_categories] = NewsCategory.all
+    
+    # News articles
+    @category[:is_national] = params[:is_national].blank? || params[:is_national].to_s.downcase == "true"
+    if @category[:is_national] && current_user&.is_national_news?
+      @category[:news_articles] = NewsArticle.joins(:news_categories).where(news_articles: { country: current_user.country }, news_categories: { id: news_category.id }).order(published_at: :desc).page(params[:page]).per(16)
+    else
+      @category[:news_articles] = NewsArticle.joins(:news_categories).where(news_categories: { id: news_category.id }).order(published_at: :desc).page(params[:page]).per(16)
+    end
   end
 end
