021b59e84720eea227a833c86c189c6b13465724
Dmytro Ababii <ababiids@gmail.com>
2021-03-04 13:57:32 +0200

    Fix wrong changes

--- app/controllers/forgot_password_controller.rb
+++ app/controllers/forgot_password_controller.rb
@@ -3,7 +3,7 @@ class ForgotPasswordController < ApplicationController
   
   # GET /forgot_password/security_question
   def security_question
-    user = User.find_by(email: params[:email])
+    user = User.find_by(email: user_params[:email])
     unless user
       render json: { errors: ['Record not found.'] }, status: :not_found
       return
@@ -18,4 +18,9 @@ class ForgotPasswordController < ApplicationController
     security_question_json = UserSecurityQuestionSerializer.new(user_security_question).as_json
     render json: { security_question: security_question_json, email: user.email }, status: :ok
   end
+  
+  private
+    def user_params
+      params.require(:user).permit(:email)
+    end
 end
