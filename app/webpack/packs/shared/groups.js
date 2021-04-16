3564f895d79b9b537263a5d6ce39ca56586e3e7d
Dmytro Ababii <ababiids@gmail.com>
2021-04-16 19:58:30 +0300

    Add code to change group tree active class

--- app/webpack/packs/shared/groups.js
+++ app/webpack/packs/shared/groups.js
@@ -1,7 +1,20 @@
 $( document ).on('turbolinks:load', function() {
   
-  $(function() {
+  $('.group-wrapper').on('click', function(e) {
+    // Prevent calling click event on all parent ul,li elements
+    e.stopPropagation()
     
-  });
+    // Click on Dropdown button do:
+    if( !$(e.target).is('.main-content') ) { return }
+    
+    // Subscribe elements
+    let mainContentEl = $(this).children('.main-content')
+    
+    if (mainContentEl.hasClass('active')) {
+      mainContentEl.removeClass('active')
+    } else {
+      mainContentEl.addClass('active')
+    }
+  })
   
 })
