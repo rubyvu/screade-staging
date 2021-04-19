230e6e3120c57a312a2c6220856ade8599d9bc0b
Dmytro Ababii <ababiids@gmail.com>
2021-04-19 11:14:00 +0300

    Add endpoints for UserTopicSubscriptions to Web

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,12 +10,19 @@ class GroupsController < ApplicationController
   
   # POST /groups/:id/subscribe
   def subscribe
-    
+    subscription = UserTopicSubscription.new(source: @group, user: current_user)
+    if subscription.save
+      render json: { success: true }, status: :ok
+    else
+      render json: { success: false }, status: :ok
+    end
   end
   
   # POST /groups/:id/unsubscribe
   def unsubscribe
-    
+    subscription = UserTopicSubscription.find_by!(source: @group, user: current_user)
+    subscription.destroy
+    render json: { success: true }, status: :ok
   end
   
   private
