d618ef339c99ad2822922f35a42efcb15340768c
Dmytro Ababii <ababiids@gmail.com>
2021-04-28 17:52:15 +0300

    Dry code for 12/24 format

--- app/webpack/packs/application.js
+++ app/webpack/packs/application.js
@@ -31,6 +31,7 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 window.$ = window.jQuery = jQuery;
+window.isTwelveHoursFormat = isTwelveHoursFormat;
 
 const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
