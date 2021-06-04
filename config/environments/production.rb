98ae4013edbbe715246a4c7574609d7de7f709a2
Dmytro Ababii <ababiids@gmail.com>
2021-06-04 15:55:49 +0300

    Add config for WebSockets on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
   
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://www.screade.com/cable'
+  config.action_cable.allowed_request_origins = ['http://www.screade.com', 'https://www.screade.com', 'http://screade.com', 'https://screade.com']
   
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
