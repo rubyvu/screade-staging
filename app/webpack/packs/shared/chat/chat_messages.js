63d8fa6bf8305337129c18a35ad6be87ddb421b2
Dmytro Ababii <ababiids@gmail.com>
2021-07-01 16:15:53 +0300

    Add Message/Record switcher to the Chat

--- app/webpack/packs/shared/chat/chat_messages.js
+++ app/webpack/packs/shared/chat/chat_messages.js
@@ -21,3 +21,27 @@ $( document ).on('turbolinks:load', function() {
     }, 800)
   })
 })
+
+$(document).on('ajax:success', 'a[id^=chat-element-]', function() {
+  // Message/Audio switcher
+  let submitButton = $('#chat-message-submit')
+  let recordButton = $('#chat-start-record-button')
+  let messageInput = $('#chat_message_text')
+  
+  messageInput.on('input', function() {
+    if ($(this).val().length > 0) {
+      submitButton.addClass('active')
+      recordButton.removeClass('active')
+    } else {
+      submitButton.removeClass('active')
+      recordButton.addClass('active')
+    }
+  });
+  
+  // Clear message input field after submit
+  $('#new_chat_message').on('ajax:success', function() {
+    messageInput.val('')
+    submitButton.removeClass('active')
+    recordButton.addClass('active')
+  })
+})
