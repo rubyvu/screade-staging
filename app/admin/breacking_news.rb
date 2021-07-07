533bf4e1e037c74b4ea047a11afe5f6163228776
Dmytro Ababii <ababiids@gmail.com>
2021-07-07 13:08:52 +0300

    Add Admin posts table to BreakingNews panel

--- app/admin/breacking_news.rb
+++ app/admin/breacking_news.rb
@@ -12,6 +12,50 @@ ActiveAdmin.register BreakingNews do
     actions
   end
   
+  show do
+    attributes_table do
+      row :id
+      row :post
+      row :created_at
+      row :updated_at
+    end
+  
+    panel 'Admin Posts' do
+      table_for Post.where(user: User.find_by(username: 'admin.screade')).order(updated_at: :asc) do
+        column 'Id' do |post|
+          post.id
+        end
+        
+        column 'Id' do |post|
+          if post.image.present?
+            image_tag post.image.url, class: 'admin-index-image'
+          else
+            image_pack_tag('media/images/placeholders/placeholder-news.png', class: 'admin-index-image')
+          end
+        end
+        
+        column 'Title' do |post|
+          link_to post.title, edit_admin_post_path(post)
+        end
+        
+        column 'Description' do |post|
+          post.description
+        end
+        
+        column 'Is Approved' do |post|
+          post.is_approved
+        end
+        
+        column 'Created At' do |post|
+          post.created_at
+        end
+        column 'Updated At' do |post|
+          post.updated_at
+        end
+      end
+    end
+  end
+  
   form do |f|
     f.inputs do
       f.input :post_id, label: 'Post', as: :select, collection: Post.where(user: User.find_by(username: 'admin.screade')).order(title: :desc)
@@ -19,5 +63,4 @@ ActiveAdmin.register BreakingNews do
     
     f.actions
   end
-  
 end
