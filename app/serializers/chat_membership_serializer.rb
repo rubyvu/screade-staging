3fffd282d365d751f3360f3321006f78a3a48f79
Dmytro Ababii <ababiids@gmail.com>
2021-07-08 16:47:10 +0300

    Update ChatMembershipSerializer

--- app/serializers/chat_membership_serializer.rb
+++ app/serializers/chat_membership_serializer.rb
@@ -2,6 +2,7 @@ class ChatMembershipSerializer < ActiveModel::Serializer
     
   attribute :id
   attribute :role
+  attribute :unread_messages_count
   attribute :user
   def user
     UserProfileSerializer.new(object.user).as_json
