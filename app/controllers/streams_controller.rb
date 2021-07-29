0445bbac70445114961752ed55ba6449ed136489
Dmytro Ababii <ababiids@gmail.com>
2021-07-29 18:13:40 +0300

    Add Stream#index styles for Web

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -2,7 +2,7 @@ class StreamsController < ApplicationController
   
   # GET /posts
   def index
-    
+    @streams = Stream.where(user: current_user).order(updated_at: :desc).page(params[:page]).per(30)
   end
   
   # GET /posts
