f2f0a05cee837663006f8dea2876c25fbf152faa
Maksym Skliarov <maksym.skliarov@gmail.com>
2021-12-09 01:03:48 +0200

    Language improvement

--- spec/jobs/create_new_notification_job_spec.rb
+++ spec/jobs/create_new_notification_job_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe CreateNewNotificationJob, type: :job do
     @recipient = FactoryBot.create(:user, country: @country, user_security_question: @user_security_question)
   end
   
-  it 'should create Notification for Comment' do
+  it 'should create Notification for shared Comment' do
     comment = FactoryBot.create(:comment, user: @user, source: @news_article)
     shared_record = FactoryBot.create(:shared_record, sender: @user, shareable: comment, users: [@recipient])
     
@@ -30,7 +30,7 @@ RSpec.describe CreateNewNotificationJob, type: :job do
     expect(notification.is_shared).to eq(true)
   end
   
-  it 'should create Notification for NewsArticle' do
+  it 'should create Notification for shared NewsArticle' do
     shared_record = FactoryBot.create(:shared_record, sender: @user, shareable: @news_article, users: [@recipient])
     
     perform_enqueued_jobs do
@@ -45,7 +45,7 @@ RSpec.describe CreateNewNotificationJob, type: :job do
     expect(notification.is_shared).to eq(true)
   end
   
-  it 'should create Notification for Post' do
+  it 'should create Notification for shared Post' do
     post = FactoryBot.create(:post, source: @topic, user: @user)
     shared_record = FactoryBot.create(:shared_record, sender: @user, shareable: post, users: [@recipient])
     
